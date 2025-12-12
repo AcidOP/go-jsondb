a0deeae72b6c6dc508948541baba063c519c81dd
Zeeshan <zeeshanalivr46@gmail.com>
2025-12-12 16:42:19 +0530

    refactor: comments

--- types/types.go
+++ types/types.go
@@ -4,10 +4,10 @@ import "encoding/json"
 
 // Entry represents a stored record in a collection file.
 type Entry struct {
-	ID  string          `json:"_id"`
-	TS  int64           `json:"ts"`
-	Op  string          `json:"op"` // insert, delete, update
-	Doc json.RawMessage `json:"doc"`
+	ID  string          `json:"_id"` // Primary key
+	TS  int64           `json:"ts"`  // Unix timestamp in nanoseconds
+	Op  string          `json:"op"`  // insert, delete, update
+	Doc json.RawMessage `json:"doc"` // doc content in JSON format. Nil for delete operations.
 }
 
 // Collection models the on-disk structure: an array of entries.
